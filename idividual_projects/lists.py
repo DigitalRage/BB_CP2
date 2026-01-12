6d7c29e9bd571566b8d4f96d2db25badbff3228f
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-12 09:38:55 -0700

    YEs

---
+++ idividual_projects/lists.py
@@ -0,0 +1,26 @@
+# BB 1st Types of Lists Notes
+
+siblings = ["Alex", "Katie", "Andrew", "Tia", "Treyson", "Xavier", "Jake"]
+
+print(siblings[3])
+siblings[-2]="DOW"
+
+print(siblings)
+
+fruit = ("apple", "orange", "peach" "kiwi", "raspberry")
+home = (0,0)
+x,y = home
+
+#fruit[3] = "pinapple"
+print(x)
+
+#set
+colors = {"Orange", "Purple", "Green", "Blue", "Yellow", "Red"}
+
+print(colors)
+colors.add("Pink")
+colors.remove("")
+for dingus in colors: 
+    if dingus == "Orange": 
+        print("fruit")
+    print(dingus)
