c0d2e8e6c545c9d10d42fff0d7475ee8212180f4
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-14 09:52:17 -0700

    Yes

---
+++ idividual_projects/personal_library.py
@@ -0,0 +1,23 @@
+# BB 1st Personal Library Project
+#choice function
+def viable(num): 
+    if choice.isdigit(): #Checks if the option is available
+        choice = int(choice)
+    return num 
+
+def add(): #Function to add a new item
+    plus = input("")
+    games.append()
+    return
+def search(): #Function to search the items
+def item():#Function to remove an item
+#Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+def main(): #make function to run main project   
+    games = [{mario: "Mario","Nintendo","Shigeru Miyamoto"}; []]
+    choice = (("What will you choose?"), ("1. View"), ("2. Add"),("3. Remove")("4. Search"),("5. Exit"), ("Type the number: "))
+    select = input(choice[0],choice[1],choice[2],choice[3],choice[4],choice[5],choice[6])# Person Choice
+    action = viable(select)
+
+    
+if __name__ == "__main__": 
+    main()
