2a890c8f316ec119c5dc0c8621182869ef42ace2
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-22 09:51:53 -0700

    Yes

---
+++ idividual_projects/what's_the _password.py
@@ -0,0 +1,13 @@
+#BB 1st Random Password Generator Project
+#Make main function
+def main(): 
+    return
+
+#asks the user for amount of characters
+#asks what types of characters to use
+#use ascii values for random password generator
+#put the generated characters in a tuple
+#return the tuple
+
+#Run main
+main()
